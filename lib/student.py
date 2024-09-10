74d27ac554bcea3977e424f6f1d5c42b3db5b4c3
Samson Njai <njaisamson@gmail.com>
2024-09-10 12:37:56 +0300

    Adds the required functionalities

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,15 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name, knowledge=None):
+        super().__init__(first_name, last_name)
+        if knowledge is None:
+            self.knowledge = []  # Initialize knowledge as an empty list
+        else:
+            self.knowledge = knowledge
+
+    def learn(self, text):
+        if isinstance(text, str):
+            self.knowledge.append(text)  # Add the string to the knowledge list
+        else:
+            raise ValueError("Knowledge must be a string")
